aad2ea66d658afe21086b09994d32ecf0b8661d2
Raissa <95766853+raissagd@users.noreply.github.com>
2023-08-01 11:37:06 -0300

    Firebase config

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAg0bX063et2e3Msitv_d7l-yZR1nABLSw",
+  authDomain: "miniblog-42ee2.firebaseapp.com",
+  projectId: "miniblog-42ee2",
+  storageBucket: "miniblog-42ee2.appspot.com",
+  messagingSenderId: "603154002186",
+  appId: "1:603154002186:web:ced29a08f47c623d488af3",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
