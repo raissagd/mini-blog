d6a2053d2540f7d187a42309a0aa5660b78891bb
Raissa <95766853+raissagd@users.noreply.github.com>
2023-08-06 09:39:02 -0300

    Estilos gerais e CSS do formulário

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,15 +13,31 @@ const Navbar = () => {
           <li>
             <NavLink
               to="/"
-              className={(isActive) => (isActive ? styles.active : "")}
+              className={({ isActive }) => (isActive ? styles.active : "")}
             >
               Home
             </NavLink>
           </li>
           <li>
             <NavLink
+              to="/login"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Entrar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/register"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Cadastrar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
               to="/about"
-              className={(isActive) => (isActive ? styles.active : "")}
+              className={({ isActive }) => (isActive ? styles.active : "")}
             >
               Sobre
             </NavLink>
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,7 @@
+import styles from "./Login.module.css";
+
+const Login = () => {
+  return <h2>Login</h2>;
+};
+
+export default Login;
