abd884a3f340dea73fedabf5bb8173aa003943b6
Raissa <95766853+raissagd@users.noreply.github.com>
2023-08-08 19:39:05 -0300

    Exibindo resultados da busca

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -4,14 +4,31 @@ import styles from "./Search.module.css";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
 
+// components
+import PostDetail from "../../components/PostDetail";
+
+import { Link } from "react-router-dom";
+
 const Search = () => {
   const query = useQuery();
   const search = query.get("q");
 
+  const { documents: posts } = useFetchDocuments("posts", search);
+
   return (
-    <div>
+    <div className={styles.search_container}>
       <h2>Search</h2>
-      <p>{search}</p>
+      <div>
+        {posts && posts.length === 0 && (
+          <>
+            <p>Não foram encontrados posts a partir da sua busca...</p>
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
